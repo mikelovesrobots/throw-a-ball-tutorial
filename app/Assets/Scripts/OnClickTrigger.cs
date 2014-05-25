f74b1c7e4f8292e37e0d29a541412730752e5cef
Mike Judge <mikelovesrobots@gmail.com>
2014-05-24 20:31:30 -0700

    Add OnClickTrigger script

---
+++ app/Assets/Scripts/OnClickTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnClickTrigger : MonoBehaviour {
+  public float MinTimeBetweenPresses = 1f;
+  public ActionBase Action;
+  private bool isDisabled = false;
+
+  void OnClick() {
+    if (!isDisabled) {
+      Action.Act();
+      if (ShouldWaitBetweenPresses) {
+        isDisabled = true;
+        StartCoroutine(WaitThenRespondToClicksAgain());
+      }
+    }
+  }
+
+  private IEnumerator WaitThenRespondToClicksAgain() {
+    yield return new WaitForSeconds(MinTimeBetweenPresses);
+    isDisabled = false;
+  }
+
+  private bool ShouldWaitBetweenPresses {
+    get { return MinTimeBetweenPresses > 0f; }
+  }
+}
