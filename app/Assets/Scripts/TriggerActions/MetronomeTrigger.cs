8308ab5e0d69c3d1feae8555318836412dfacf16
Mike Judge <mikelovesrobots@gmail.com>
2015-01-08 20:27:45 -0800

    Add a metronome trigger to TriggerActions library

---
+++ app/Assets/Scripts/TriggerActions/MetronomeTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MetronomeTrigger : MonoBehaviour {
+  public float DelayInSeconds = 0.5f;
+  public ActionBase Action;
+
+  void Start () {
+    StartCoroutine(DelayThenTick());
+  }
+
+  IEnumerator DelayThenTick() {
+    yield return new WaitForSeconds(DelayInSeconds);
+    Action.Act();
+    StartCoroutine(DelayThenTick());
+  }
+}
